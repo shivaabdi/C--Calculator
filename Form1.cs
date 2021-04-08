b0da5997564b24f4670d46514764d44cae02799b
shivaabdi <shivaabdiazari@gmail.com>
2021-04-08 21:49:14 +0300

    second commit

---
+++ Form1.cs
@@ -0,0 +1,232 @@
+﻿using LinqToTwitter;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Clear();
+            }
+            Button button = (Button)sender;
+            textBox1.Text += button.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Clear();
+            }
+            Button button = (Button)sender;
+            textBox1.Text += button.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Clear();
+            }
+            Button button = (Button)sender;
+            textBox1.Text += button.Text;
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Clear();
+            }
+            Button button = (Button)sender;
+            textBox1.Text += button.Text;
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Clear();
+            }
+            Button button = (Button)sender;
+            textBox1.Text += button.Text;
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Clear();
+            }
+            Button button = (Button)sender;
+            textBox1.Text += button.Text;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Clear();
+            }
+            Button button = (Button)sender;
+            textBox1.Text += button.Text;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Clear();
+            }
+            Button button = (Button)sender;
+            textBox1.Text += button.Text;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Clear();
+            }
+            Button button = (Button)sender;
+            textBox1.Text += button.Text;
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Clear();
+            }
+            Button button = (Button)sender;
+            textBox1.Text += button.Text;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Clear();
+            }
+            Button button = (Button)sender;
+            textBox1.Text += button.Text;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Clear();
+            }
+            x = double.Parse(textBox1.Text);
+            textBox1.Clear();
+            operation = "devide";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Clear();
+            }
+            x = double.Parse(textBox1.Text);
+            textBox1.Clear();
+            operation = "mul";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Clear();
+            }
+            x = double.Parse(textBox1.Text);
+            textBox1.Clear();
+            operation = "sub";
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Clear();
+            }
+            x = double.Parse(textBox1.Text);
+            textBox1.Clear();
+            operation = "add";
+            
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+        string operation;
+        double y;
+        double x;
+        double result;
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "0")
+            {
+                y = double.Parse(textBox1.Text);
+                switch (operation)
+                {
+                    case "add":
+                        textBox1.Text = (x + y).ToString("0.0");
+                        break;
+                    case "sub":
+                        textBox1.Text = (x - y).ToString("0.0");
+                        break;
+                    case "mul":
+                        textBox1.Text = (x * y).ToString("0.0");
+                        break;
+                    case "devide":
+                        textBox1.Text = (x / y).ToString("0.0");
+                        break;
+
+
+
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Clear();
+            }
+            Button button = (Button)sender;
+            textBox1.Text += button.Text;
+        }
+    }
+}
---
+++ packages/linqtotwitter.5.1.2/ReadMe.txt
@@ -0,0 +1,33 @@
+# Welcome to LINQ to Twitter!
+
+## Release Notes:
+
+* Full documentation in the GitHub Wiki:
+    - https://github.com/JoeMayo/LinqToTwitter/wiki 
+
+* Demos included in downloadable source code in the /Samples folder. 
+	- ConsoleSamples covers all APIs and there are other technology specific demos too.
+	
+* How to use with Xamarin.Android: 
+	- http://geekswithblogs.net/WinAZ/archive/2013/11/14/linq-to-twitter-runs-xamarin.android.aspx
+	
+* How to use with Xamarin.iOS: 
+    - http://geekswithblogs.net/WinAZ/archive/2013/12/06/linq-to-twitter-runs-on-xamarin.ios.aspx
+
+* You can find help on Stack Overflow:
+	- https://stackoverflow.com/questions/tagged/linq-to-twitter
+
+* Please visit the GitHub issues page if you've found a bug or would like to contribute:
+    - https://github.com/JoeMayo/LinqToTwitter/issues 
+
+## Supported Platforms:
+
+	"net48",
+    "uap10.0",
+	"xamarin.ios",
+	"xamarin.mac",
+    "monoandroid",
+    "netcoreapp3.1",
+    "netstandard2.0"
+
+### Follow @JoeMayo and @linq2Twitr on Twitter for the latest news.
